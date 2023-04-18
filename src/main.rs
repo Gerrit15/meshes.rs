c00e460959a92f6ee717e1a6cd512b7a5120161b
Gerrit15 <overlorde159@gmail.com>
2023-04-18 13:49:41 -0400

    testing ground

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use triangle::Triangle;
 use object::Object;
 
 fn main() {
-    let a = Object::new_tetrahedron(1.0, None, None);
-    println!("{}", a);
+    let vert1 = Vec3::new(0.0, 3.0, 0.0);
+    let vert2 = Vec3::new(0.0, 0.0, 0.0);
+    let vert3 = Vec3::new(0.0, 0.0, 1.0);
+    let face = Triangle::new(&vert1, &vert2, &vert3);
+    let point = Vec3::new(1.0, 1.5, 0.5);
+    println!("{}", face.closest_point(point));
 }
